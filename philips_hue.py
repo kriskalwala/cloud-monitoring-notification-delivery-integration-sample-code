1e72b3a478672faa7fc66ee3c3b1dc3349c560c9
April Xie <aprilxie@google.com>
2020-06-12 17:18:21 -0700

    added TODO for config loading

--- philips_hue.py
+++ philips_hue.py
@@ -12,9 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Philips Hue API Documentation: https://developers.meethue.com/develop/hue-api/
+
 import os
 import requests
 import json
+
+# TODO: make config loading centralized
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -25,7 +29,9 @@ def set_color(light_id, hue):
     """Sets the color of a Philips Hue light to a specified hue value.
 
     Args:
-        light_id: The id of the light to set the color for.
-        hue: Hue of the light.
+        light_id: The id to pass to the Philips Hue API to specify the light to set a color for.
+        hue: Hue of the light. Takes values from 0 to 65535. 
+            Programming 0 and 65535 would mean that the light will resemble 
+            the color red, 21845 for green and 43690 for blue.
     """
     r = requests.put(url = f'{URL}/lights/{light_id}/state', data=json.dumps({"on": True, "hue": hue}))
