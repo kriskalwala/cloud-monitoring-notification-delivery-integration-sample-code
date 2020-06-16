895807028b72405d93810bb93dc82741fe439507
April Xie <aprilxie@google.com>
2020-06-16 14:12:45 -0700

    created PhilipsHueClient class

--- philips_hue.py
+++ philips_hue.py
@@ -18,20 +18,22 @@ import os
 import requests
 import json
 
-# TODO: make config loading centralized
-from dotenv import load_dotenv
 
-load_dotenv()
-URL = os.environ.get("philips-url")
+class PhilipsHueClient(object):
+    def __init__(self, api_url):
+        self.api_url = api_url
+        
 
+    def set_color(self, light_id, hue):
+        """Sets the color of a Philips Hue light to a specified hue value.
 
-def set_color(light_id, hue):
-    """Sets the color of a Philips Hue light to a specified hue value.
+        Args:
+            light_id: The id to pass to the Philips Hue API to specify the light to set a color for.
+            hue: Hue of the light. Takes values from 0 to 65535. 
+                Programming 0 and 65535 would mean that the light will resemble 
+                the color red, 21845 for green and 43690 for blue.
 
-    Args:
-        light_id: The id to pass to the Philips Hue API to specify the light to set a color for.
-        hue: Hue of the light. Takes values from 0 to 65535. 
-            Programming 0 and 65535 would mean that the light will resemble 
-            the color red, 21845 for green and 43690 for blue.
-    """
-    r = requests.put(url = f'{URL}/lights/{light_id}/state', data=json.dumps({"on": True, "hue": hue}))
+        Returns:
+            HTTP Response from the Philips Hue API.
+        """
+        return requests.put(url = f'{self.api_url}/lights/{light_id}/state', data=json.dumps({"on": True, "hue": hue}))
