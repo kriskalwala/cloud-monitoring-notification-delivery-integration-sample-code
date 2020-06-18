5ba706dc936c4c9386cf810824e6afc5b7ab26fd
April Xie <aprilxie@google.com>
2020-06-18 15:08:44 -0700

    separation of concerns for pubsub message data vs notification messages

--- main.py
+++ main.py
@@ -12,21 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# The code in this module is based on https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/run/pubsub/main.py. 
-# See https://cloud.google.com/run/docs/tutorials/pubsub for the accompanying Cloud Run/PubSub solutions guide.
+# The code in this module is based on
+# https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/run/pubsub/main.py.
+# See https://cloud.google.com/run/docs/tutorials/pubsub for the accompanying
+# Cloud Run/PubSub solutions guide.
+
+"""Runs Cloud Monitoring Notification Integration app with Flask."""
 
 # [START run_pubsub_server_setup]
-import base64
-import binascii
 import os
 import json
-import philips_hue
 
 from flask import Flask, request
-from config import configs
-import google_monitoring
-
 from dotenv import load_dotenv
+
+import philips_hue
+from config import configs
+import pubsub
 
 
 load_dotenv()
@@ -36,32 +38,35 @@ env = os.environ.get('FLASK_APP_ENV', 'default')
 app.config.from_object(configs[env])
 # [END run_pubsub_server_setup]
 
-        
+
 # [START run_pubsub_handler]
 @app.route('/', methods=['POST'])
 def index():
-    pubsub_envelope = request.get_json()
-    
-    # parse the Pub/Sub notification
+    pubsub_received_message = request.get_json()
+
+    # parse the Pub/Sub data
     try:
-        notification_dict = google_monitoring.parse_notification_from_pubsub_envelope(pubsub_envelope)
-    except google_monitoring.NotificationParseError as e:
-        print(e)
-        return (e.message, 400)
-    
-    # parse the incident from the notification
-    try:
-        incident = google_monitoring.parse_incident_from_notification(notification_dict)
-        print(incident)
-    except google_monitoring.IncidentParseError as e:
+        pubsub_data_string = pubsub.parse_data_from_message(pubsub_received_message)
+    except pubsub.DataParseError as e:
         print(e)
         return (e.message, 400)
 
-    philips_hue_client = philips_hue.PhilipsHueClient(app.config['BRIDGE_IP_ADDRESS'], app.config['USERNAME'])
-    
+    # load the notification from the data
     try:
-        response = philips_hue.trigger_hue_from_incident(philips_hue_client, incident, app.config['LIGHT_ID'])  
-    except ValueError as e:
+        monitoring_notification_dict = json.loads(pubsub_data_string)
+    except json.JSONDecodeError as e:
+        msg = 'notification could not be decoded to a JSON'
+        print(msg)
+        return (msg, 400)
+
+
+    philips_hue_client = philips_hue.PhilipsHueClient(app.config['BRIDGE_IP_ADDRESS'],
+                                                      app.config['USERNAME'])
+
+    try:
+        response = philips_hue.trigger_light_from_monitoring_notification(
+            philips_hue_client, monitoring_notification_dict, app.config['LIGHT_ID'])
+    except philips_hue.Error as e:
         print(e)
         return (e.message, 400)
 
