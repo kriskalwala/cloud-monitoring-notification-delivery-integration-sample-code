e37a43f4bc49ed4db2a262d84a8adffe142a00df
April Xie <aprilxie@google.com>
2020-07-10 16:14:24 -0700

    removed interpolation

--- modules/cloud_build_trigger/main.tf
+++ modules/cloud_build_trigger/main.tf
@@ -1,11 +1,11 @@
 resource "google_cloudbuild_trigger" "filename-trigger" {
   trigger_template {
-    branch_name = "${var.branch}"
-    repo_name   = "${var.repo}"
+    branch_name = var.branch
+    repo_name   = var.repo
   }
 
   substitutions = {
-    _IMAGE_NAME = "${var.image_name}"
+    _IMAGE_NAME = var.image_name
   }
 
   filename = "cloudbuild.yaml"
