7f5f49a9eff111ce00633a48dad478c4f9d31676
April Xie <aprilxie@google.com>
2020-07-10 16:09:28 -0700

    added cloudbuild trigger module

---
+++ modules/cloud_build_trigger/main.tf
@@ -0,0 +1,12 @@
+resource "google_cloudbuild_trigger" "filename-trigger" {
+  trigger_template {
+    branch_name = "${var.branch}"
+    repo_name   = "${var.repo}"
+  }
+
+  substitutions = {
+    _IMAGE_NAME = "${var.image_name}"
+  }
+
+  filename = "cloudbuild.yaml"
+}
---
+++ modules/cloud_build_trigger/variables.tf
@@ -0,0 +1,11 @@
+variable "branch" {
+  default = "terraform-cloud-build-trigger"
+}
+
+variable "repo" {
+  default = "googleinterns/cloud-monitoring-notification-delivery-integration-sample-code"
+}
+
+variable "image_name" {
+  default = "cloud-run-pubsub-service"
+}
