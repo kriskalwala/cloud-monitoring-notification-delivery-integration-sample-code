2f4d7491abd6b3b5930fb8d13805573c4db479f3
April Xie <aprilxie@google.com>
2020-07-21 14:53:51 -0700

    fix key issue for members

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -36,6 +36,6 @@ resource "google_project_iam_binding" "project" {
   role    = "roles/iam.serviceAccountTokenCreator"
   
   members = [
-    "service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+    "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
   ]
 }
