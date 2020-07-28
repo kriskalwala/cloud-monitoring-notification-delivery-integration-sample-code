11e4445c9b1aa76d1a3f6a118a444a9e18ebc490
Ruben Hambardzumyan <rhamb@google.com>
2020-07-28 18:09:20 -0500

    refactor script code and add instructions

--- script/jira_authentication_script.py
+++ script/jira_authentication_script.py
@@ -1,15 +1,10 @@
-import requests
-from oauthlib.oauth1 import SIGNATURE_RSA
+import argparse
 from requests_oauthlib import OAuth1Session
-from jira.client import JIRA
+from oauthlib.oauth1 import SIGNATURE_RSA
 from Crypto.PublicKey import RSA
 from google.cloud import secretmanager
 from google.api_core.exceptions import AlreadyExists
 
-def read(file_path):
-    """ Read a file and return it's contents. """
-    with open(file_path) as f:
-        return f.read()
 
 def create_secret(client, project_id, secret_id):
     """
@@ -18,17 +13,12 @@ def create_secret(client, project_id, secret_id):
     secret material.
     """
 
-    # Build the resource name of the parent project.
     parent = client.project_path(project_id)
-
-    # Create the secret.
     response = client.create_secret(parent, secret_id, {
         'replication': {
             'automatic': {},
         },
     })
-
-    # Print the new secret name.
     print('Created secret: {}'.format(response.name))
 
 
@@ -37,128 +27,129 @@ def add_secret_version(client, project_id, secret_id, payload):
     Add a new secret version to the given secret with the provided payload.
     """
     
-    # Build the resource name of the parent secret.
     parent = client.secret_path(project_id, secret_id)
 
-    # Convert the string payload into a bytes. This step can be omitted if you
-    # pass in bytes instead of a str for the payload argument.
     if not isinstance(payload, bytes):
         payload = payload.encode('UTF-8')
 
-    # Add the secret version.
     response = client.add_secret_version(parent, {'data': payload})
-
-    # Print the new secret version name.
     print('Added secret version: {}'.format(response.name))
 
 
-private_key = RSA.generate(2048)
-private_key_pem = private_key.exportKey('PEM')
-with open("private.pem", "wb") as f:
-    f.write(private_key_pem)
+parser = argparse.ArgumentParser(description=('Setup Jira Server OAuth'))
 
-public_key = private_key.publickey()
-public_key_pem = public_key.exportKey('PEM')
-with open("public.pem", "wb") as f:
-    f.write(public_key_pem)
+parser.add_argument('project_id',
+                    help='id of Google Cloud project to store secrets in')
 
-while input("Press any key to continue..."):
-    pass
+parser.add_argument('jira_url',
+                    help='URL of the Jira Server to setup OAuth for')
 
-# The Consumer Key created while setting up the "Incoming Authentication" in
-# JIRA for the Application Link.
-CONSUMER_KEY = 'OauthKey'
+parser.add_argument('-m',
+                    action='store_true',
+                    help='use already generated private/public RSA keys called private.pem and public.pem')
 
-# The contents of the rsa.pem file generated (the private RSA key)
-RSA_KEY = private_key_pem
-
-# The URLs for the JIRA instance
-JIRA_SERVER = 'http://localhost:8080'
-REQUEST_TOKEN_URL = JIRA_SERVER + '/plugins/servlet/oauth/request-token'
-AUTHORIZE_URL = JIRA_SERVER + '/plugins/servlet/oauth/authorize'
-ACCESS_TOKEN_URL = JIRA_SERVER + '/plugins/servlet/oauth/access-token'
+args = parser.parse_args()
 
 
-# Step 1: Get a request token
+# Create or read in RSA public and private keys
+if args.m:
+    with open('private.pem') as f:
+        private_key_pem = f.read()
 
-oauth = OAuth1Session(CONSUMER_KEY, signature_type='auth_header',
-                      signature_method=SIGNATURE_RSA, rsa_key=RSA_KEY,
-                      verifier="jira_verifier")
-request_token = oauth.fetch_request_token(REQUEST_TOKEN_URL)
+    with open('public.pem') as f:
+        public_key_pem = f.read()
 
-print("STEP 1: GET REQUEST TOKEN")
-print("  oauth_token={}".format(request_token['oauth_token']))
-print("  oauth_token_secret={}".format(request_token['oauth_token_secret']))
-print("\n")
+    print("RSA public and private keys loaded")
+else:
+    private_key = RSA.generate(2048)
+    private_key_pem = private_key.exportKey('PEM')
+    with open('private.pem', 'wb') as f:
+        f.write(private_key_pem)
 
+    public_key = private_key.publickey()
+    public_key_pem = public_key.exportKey('PEM')
+    with open('public.pem', 'wb') as f:
+        f.write(public_key_pem)
 
-# Step 2: Get the end-user's authorization
-
-print("STEP2: AUTHORIZATION")
-print("  Visit to the following URL to provide authorization:")
-print("  {}?oauth_token={}".format(AUTHORIZE_URL, request_token['oauth_token']))
-print("\n")
-
-while input("Press any key to continue..."):
-    pass
-
-
-# Step 3: Get the access token
-
-access_token = oauth.fetch_access_token(ACCESS_TOKEN_URL)
-oauth_token = access_token['oauth_token']
-oauth_token_secret = access_token['oauth_token_secret']
-
-print("STEP2: GET ACCESS TOKEN")
-print("  oauth_token={}".format(oauth_token))
-print("  oauth_token_secret={}".format(oauth_token_secret))
-print("\n")
+    print("RSA public and private keys created")
 
 
-# SECRET MANAGER
 
-project_id = "alertmanager-2020-intern-r"
+# Setup Jira Oauth
+consumer_key = 'CloudMonitoringOauthKey'
 
+print(f"""\nComplete the following steps:
+1. In Jira, navigate to Jira Settings > Applications > Application Links
+2. In the 'Enter the URL of the application you want to link' field, enter http://example.com/
+3. On the first screen of the 'Link applications' dialog, select the 'Create incoming link' checkbox and click 'Continue'
+4. On next screen of the 'Link applications' dialog, enter the following consumer details:
+    * Consumer Key: {consumer_key}
+    * Consumer Name: Cloud Monitoring App
+    * Public Key:\n{public_key_pem}
+5: Click 'Continue'
+
+(Note the previous steps are based off of the instructions at https://developer.atlassian.com/server/jira/platform/oauth/#create-an-application-link)\n""")
+
+input('Once complete, press "Enter" to proceed\n')
+
+oauth = OAuth1Session(consumer_key,
+                      signature_method=SIGNATURE_RSA,
+                      rsa_key=private_key_pem,
+                      signature_type='auth_header',
+                      verifier='jira_verifier')
+
+request_token_url = f'{args.jira_url}/plugins/servlet/oauth/request-token'
+fetch_response = oauth.fetch_request_token(request_token_url)
+oauth_token = fetch_response.get('oauth_token')
+oauth_token_secret = fetch_response.get('oauth_token_secret')
+
+base_authorization_url =  f'{args.jira_url}/plugins/servlet/oauth/authorize'
+authorization_url = oauth.authorization_url(base_authorization_url)
+print(f'Go to the following URL and click allow: {authorization_url}\n')
+input('Once complete, press "Enter" to proceed\n')
+
+access_token_url = f'{args.jira_url}/plugins/servlet/oauth/access-token'
+oauth_tokens = oauth.fetch_access_token(access_token_url)
+oauth_token = oauth_tokens.get('oauth_token')
+oauth_token_secret = oauth_tokens.get('oauth_token_secret')
+
+with open('jira_access_token.txt', 'w') as f:
+        f.write(oauth_token)
+print('Jira access token stored in jira_access_token.txt')
+
+with open('jira_access_token_secret.txt', 'w') as f:
+        f.write(oauth_token_secret)
+print('Jira access token secret stored in jira_access_token_secret.txt')
+
+
+
+# Store Oauth data necessary to authenticate Jira in Google Secret Manager
 client = secretmanager.SecretManagerServiceClient()
 
 try:
-    create_secret(client, project_id, "jira_access_token")
+    create_secret(client, project_id, 'jira_access_token')
 except AlreadyExists as e:
     print('Secret already exists: "jira_access_token"')
 
 try:
-    create_secret(client, project_id, "jira_access_token_secret")
+    create_secret(client, project_id, 'jira_access_token_secret')
 except AlreadyExists as e:
     print('Secret already exists: "jira_access_token_secret"')
 
 try:
-    create_secret(client, project_id, "jira_consumer_key")
+    create_secret(client, project_id, 'jira_consumer_key')
 except AlreadyExists as e:
     print('Secret already exists: "jira_consumer_key"')
 
 try:
-    create_secret(client, project_id, "jira_key_cert")
+    create_secret(client, project_id, 'jira_key_cert')
 except AlreadyExists as e:
     print('Secret already exists: "jira_key_cert"')
 
 
-add_secret_version(client, project_id, "jira_access_token", oauth_token)
-add_secret_version(client, project_id, "jira_access_token_secret", oauth_token_secret)
-add_secret_version(client, project_id, "jira_consumer_key", CONSUMER_KEY)
-add_secret_version(client, project_id, "jira_key_cert", private_key_pem)
+add_secret_version(client, project_id, 'jira_access_token', oauth_token)
+add_secret_version(client, project_id, 'jira_access_token_secret', oauth_token_secret)
+add_secret_version(client, project_id, 'jira_consumer_key', consumer_key)
+add_secret_version(client, project_id, 'jira_key_cert', private_key_pem)
 
-
-
-
-# Now you can use the access tokens with the JIRA client. Hooray!
-
-jira = JIRA(options={'server': JIRA_SERVER}, oauth={
-    'access_token': access_token['oauth_token'],
-    'access_token_secret': access_token['oauth_token_secret'],
-    'consumer_key': CONSUMER_KEY,
-    'key_cert': RSA_KEY
-})
-
-# print all of the project keys just as an exmaple
-for project in jira.projects():
-    print(project.key)
+print("Successfully setup Jira Authentication!")
